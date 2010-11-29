82132e8cb3071ff9f72d5ffd42efe1f95c73b457
Chris <christjb@usc.edu>
2010-11-28 21:01:57 -0800

    Added codes for special keys to protocol.

--- PRemoteDroid Protocol/src/org/pierre/remotedroid/protocol/action/UMoteRemoteAction.java
+++ PRemoteDroid Protocol/src/org/pierre/remotedroid/protocol/action/UMoteRemoteAction.java
@@ -6,6 +6,35 @@ import java.io.IOException;
 
 public class UMoteRemoteAction extends PRemoteDroidAction
 {
+	public static final byte CTRL = 0;
+	public static final byte ALT = 1;
+	public static final byte SHIFT = 2;
+	public static final byte CAPS_LOCK = 3;
+	public static final byte TAB = 4;
+	public static final byte ESC = 5;
+	public static final byte F1 = 6;
+	public static final byte F2 = 7;
+	public static final byte F3 = 8;
+	public static final byte F4 = 9;
+	public static final byte F5 = 10;
+	public static final byte F6 = 11;
+	public static final byte F7 = 12;
+	public static final byte F8 = 13;
+	public static final byte F9 = 14;
+	public static final byte F10 = 15;
+	public static final byte F11 = 16;
+	public static final byte F12 = 17;
+	public static final byte INSERT = 18;
+	public static final byte DELETE = 19;
+	public static final byte HOME = 20;
+	public static final byte PG_UP = 21;
+	public static final byte PG_DN = 22;
+	public static final byte BACKSPACE = 23;
+	public static final byte ARROW_UP = 24;
+	public static final byte ARROW_DOWN = 25;
+	public static final byte ARROW_LEFT = 26;
+	public static final byte ARROW_RIGHT = 27;
+	public static final byte WINDOWS = 28;
 	
 	public int keys[];
 	public byte specialKeys[];
