f5a665fd73d456b0777c2db770e3337850430974
Chris <christjb@usc.edu>
2010-11-30 18:19:39 -0800

    Added Debolina's code.

---
+++ PRemoteDroid Client/src/org/pierre/remotedroid/client/activity/UISelectionActivity.java
@@ -0,0 +1,54 @@
+package org.pierre.remotedroid.client.activity;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+
+public class UISelectionActivity extends Activity
+{
+	public static DBAdapter dba;
+	
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		dba = new DBAdapter(this);
+		
+		ScrollView sv = new ScrollView(this);
+		RelativeLayout rl = new RelativeLayout(this);
+		sv.addView(rl);
+		ArrayList<String> tables = new ArrayList<String>();
+		tables = dba.listTables();
+		
+		for (int i = 0; i < tables.size(); i++)
+		{
+			Log.v(this.toString(), "Table name " + tables.get(i));
+		}
+		String str[] = (String[]) tables.toArray(new String[tables.size()]);
+		
+		ArrayList<Record> results = new ArrayList<Record>();
+		for (int i = 0; i < results.size(); i++)
+			results = dba.query(str[i]);
+		
+		for (int i = 0; i < results.size(); i++)
+		{
+			Record temp = results.get(i);
+			Button btn = new Button(this);
+			btn.setText(temp.getLabel());
+			btn.setBackgroundColor(temp.getColor());
+			RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(temp.getWidth(), temp.getWidth());
+			params.leftMargin = temp.getX();
+			params.topMargin = temp.getY();
+			rl.addView(btn, params);
+			Log.v(this.toString(), "Name:" + temp.getName() + " X:" + temp.getX() + " Y: " + temp.getY() + " Width:" + temp.getWidth() + " Height:" + temp.getHeight() + " Color:" + temp.getColor() + " Label:" + temp.getLabel() + " KeyBinding:" + temp.getKeyBinding());
+		}
+		this.setContentView(sv);
+		
+	}
+}
